42d22f5fdb83c7d8d770600241789c69cc987ad6
POCHOL FLORIAN p1309216 <florian.pochol@etu.univ-lyon1.frcd>
2017-12-07 23:33:33 +0100

    init

---
+++ src/main/webapp/app/account/account.state.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('saroulemanApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('account', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
