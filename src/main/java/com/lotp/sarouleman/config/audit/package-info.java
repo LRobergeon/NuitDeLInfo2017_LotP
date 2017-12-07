accded26f697a4ded9735f952696ec02598f16be
POCHOL FLORIAN p1309216 <florian.pochol@etu.univ-lyon1.frcd>
2017-12-07 21:23:35 +0100

    remove

--- src/main/java/com/lotp/sarouleman/config/audit/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Audit specific code.
- */
-package com.lotp.sarouleman.config.audit;
--- src/main/java/com/lotp/sarouleman/repository/search/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Data Elasticsearch repositories.
- */
-package com.lotp.sarouleman.repository.search;
