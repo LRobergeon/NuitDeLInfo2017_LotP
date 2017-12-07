0dfd942226ef69bd403b619250fc1809d5ee2476
BINET DAMIEN p1205738 <alann@pas-ici.net>
2017-12-07 18:20:56 +0100

    init project

---
+++ src/main/java/com/lotp/sarouleman/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.lotp.sarouleman.repository;
+
+import com.lotp.sarouleman.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
---
+++ src/main/java/com/lotp/sarouleman/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.lotp.sarouleman.security;
+
+import com.lotp.sarouleman.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}
