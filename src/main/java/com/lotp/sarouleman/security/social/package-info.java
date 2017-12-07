a6f031104d900c966c74a9cfa198d2750973a2ea
kylaste <kylaste@yahoo.fr>
2017-12-07 23:25:24 +0100

    remove

--- src/main/java/com/lotp/sarouleman/security/social/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring social configuration.
- */
-package com.lotp.sarouleman.security.social;
--- src/main/webapp/app/app.main-aot.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { ProdConfig } from './blocks/config/prod.config';
-import { SaroulemanAppModuleNgFactory } from '../../../../target/aot/src/main/webapp/app/app.module.ngfactory';
-
-ProdConfig();
-
-platformBrowser().bootstrapModuleFactory(SaroulemanAppModuleNgFactory)
-.then((success) => console.log(`Application started`))
-.catch((err) => console.error(err));
