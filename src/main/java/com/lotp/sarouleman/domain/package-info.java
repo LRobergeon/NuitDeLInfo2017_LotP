7353db81468561dc21876873031d5ffc48ad5195
POCHOL FLORIAN p1309216 <florian.pochol@etu.univ-lyon1.frcd>
2017-12-08 03:08:10 +0100

    enfin

--- src/main/java/com/lotp/sarouleman/domain/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * JPA domain objects.
- */
-package com.lotp.sarouleman.domain;
--- src/main/java/com/lotp/sarouleman/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.lotp.sarouleman.web.rest.vm;
